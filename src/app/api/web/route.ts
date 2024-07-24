09c3ac4c700ff93a909021395da2fc4b300021d3
mfric <matijafric@gmail.com>
2024-07-24 16:52:05 +0200

    Bug fix

--- src/app/api/web/route.ts
+++ src/app/api/web/route.ts
@@ -1,7 +1,7 @@
 import { db } from "~/server/db";
 
 export async function GET(req: Request, res: Response) {
-  const posts = await db.query.posts.findMany();
+  const posts = await db.query.users.findMany();
 
   return new Response(JSON.stringify({ posts: posts }));
 }
