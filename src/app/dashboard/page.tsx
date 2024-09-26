35056a3c4007baf2d18d22001538b927ccfcb1a2
mfric <matijafric@gmail.com>
2024-09-26 19:23:39 +0200

    -Added delete icon on expense profile -Replaced flex with grid for better view -Added picture if there is no expense profiles

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,12 +2,15 @@
 
 import { PlusCircleIcon } from "@heroicons/react/24/solid";
 import { useQuery } from "@tanstack/react-query";
+import { useState, useEffect } from "react";
 
 import axios from "axios";
 import ExpenseProfileCard from "~/components/cards/expenseProfileCard";
 import { TExpenseProfile } from "~/types/types";
 
 export default function DashboardPage() {
+    const [screenWidth, setScreenWidth] = useState(0);
+
     const expenseProfilesQuery = useQuery<TExpenseProfile[]>({
         queryKey: ["getExpenseProfiles"],
         queryFn: async () => {
@@ -16,8 +19,19 @@ export default function DashboardPage() {
         },
     });
 
+    useEffect(() => {
+        const updateWidth = () => {
+            setScreenWidth(window.innerWidth);
+        };
+
+        updateWidth();
+        window.addEventListener("resize", updateWidth);
+
+        return () => window.removeEventListener("resize", updateWidth);
+    }, []);
+
     return (
-        <div>
+        <>
             <div className="flex flex-col gap-6 px-10 md:px-36 py-8 drop-shadow-md">
                 <div className="mx-auto text-2xl md:text-4xl font-semibold">
                     <span className="text-blue-600">Expense</span> profiles
@@ -28,27 +42,49 @@ export default function DashboardPage() {
                         Loading...
                     </div>
                 ) : expenseProfilesQuery.isSuccess ? (
-                    <div className="flex flex-row gap-8 h-36">
-                        {expenseProfilesQuery.data.map((expenseProfile) => (
-                            <div
-                                key={
-                                    "expenseProfile" +
-                                    expenseProfilesQuery.data.indexOf(
-                                        expenseProfile,
-                                    )
-                                }
-                            >
-                                <ExpenseProfileCard
-                                    expenseProfile={expenseProfile}
+                    expenseProfilesQuery.data.length == 0 ? (
+                        screenWidth > 1200 ? (
+                            <div className="flex justify-end lg:pr-56 pt-10">
+                                <img
+                                    className="w-auto opacity-60"
+                                    src="dashboard_web.png"
                                 />
                             </div>
-                        ))}
-                    </div>
+                        ) : (
+                            <div className="flex justify-end pr-5 pt-10">
+                                <img
+                                    className="w-auto opacity-60"
+                                    src="dashboard_mobile.png"
+                                />
+                            </div>
+                        )
+                    ) : (
+                        <div className="flex justify-center mt-8">
+                            <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-8">
+                                {expenseProfilesQuery.data.map(
+                                    (expenseProfile) => {
+                                        return (
+                                            <ExpenseProfileCard
+                                                expenseProfile={expenseProfile}
+                                                key={
+                                                    "expenseProfile" +
+                                                    expenseProfilesQuery.data.indexOf(
+                                                        expenseProfile,
+                                                    )
+                                                }
+                                            />
+                                        );
+                                    },
+                                )}
+                            </div>
+                        </div>
+                    )
                 ) : (
                     <div></div>
                 )}
             </div>
+
             <PlusCircleIcon className="w-14 fixed md:bottom-20 md:right-36 bottom-12 right-12 text-button drop-shadow-md hover:text-button/90 hover:cursor-pointer" />
-        </div>
+        </>
     );
 }
--- src/components/cards/expenseProfileCard.tsx
+++ src/components/cards/expenseProfileCard.tsx
@@ -2,7 +2,7 @@
 
 import { TExpenseProfile } from "~/types/types";
 import { Button } from "../ui/button";
-import { HeartIcon } from "@heroicons/react/24/outline";
+import { HeartIcon, TrashIcon } from "@heroicons/react/24/outline";
 
 export default function ExpenseProfileCard({
     expenseProfile,
@@ -10,21 +10,26 @@ export default function ExpenseProfileCard({
     expenseProfile: TExpenseProfile;
 }) {
     return (
-        <div
-            className={`border-2 rounded-md gap-2 flex flex-col flex-1 w-72 h-64 pb-4 drop-shadow-none`}
-        >
+        <div className="flex flex-row justify-items-start">
             <div
-                style={{ backgroundColor: expenseProfile.color ?? "" }}
-                className="mb-0 rounded-sm m-6 h-2/4"
-            />
-            <div className="text-xl font-semibold text-center p-4 h-1/4 overflow-clip">
-                {expenseProfile.name}
-            </div>
-            <div className="flex gap-2 justify-center items-center h-1/4">
-                <Button className="px-8">View</Button>
-                <HeartIcon
-                    className={`w-8 transition-colors duration-300 ${expenseProfile.favorite ? "fill-red-500 text-red-500 hover:fill-none hover:text-primary " : ""} hover:cursor-pointer hover:fill-red-500 hover:text-red-500`}
+                className={`border-2 rounded-md gap-2 flex flex-col flex-1 min-w-72 h-64 pb-4 drop-shadow-none`}
+            >
+                <div
+                    style={{ backgroundColor: expenseProfile.color ?? "" }}
+                    className="mb-0 rounded-sm m-6 h-2/4"
                 />
+                <div className="text-xl font-semibold text-center p-4 h-1/4 overflow-clip">
+                    {expenseProfile.name}
+                </div>
+                <div className="flex gap-2 justify-center items-center h-1/4">
+                    <Button className="px-8">View</Button>
+                    <HeartIcon
+                        className={`w-8 transition-colors duration-200 ${expenseProfile.favorite ? "fill-red-500 text-red-500 hover:fill-none hover:text-primary" : "hover:fill-red-500 hover:text-red-500"} hover:cursor-pointer `}
+                    />
+                </div>
+            </div>
+            <div className="flex -mr-5">
+                <TrashIcon className="w-8 my-auto bg-red-600 p-1 rounded-md -translate-x-4 hover:cursor-pointer hover:bg-red-600/80 transition-colors duration-300" />
             </div>
         </div>
     );
